5772dd994b1383e04041996236553b66c8a62673
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2022-04-27 14:09:56 -0400

    Main

---
+++ main.c
@@ -0,0 +1,23 @@
+/*
+ * main.c
+ *
+ *  Created on: Apr 27, 2022
+ *      Author: user
+ */
+
+#include "uartstdio.h"
+#include "TM4cUtils.h"
+#include "lrf.h"
+
+
+
+
+
+int main(int argc, char* argv[])
+{
+
+    initUART();
+
+
+
+}
